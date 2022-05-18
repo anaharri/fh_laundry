9addac815d63801cdc9154932c681c6560d9e8b5
anaharri <anaharringtonb@gmail.com>
2022-05-18 00:12:54 -0300

    level up getter functions

--- src/utils/getters.js
+++ src/utils/getters.js
@@ -1,26 +1,21 @@
 import { db } from "../firebaseconf.js";
-import { collection, getDocs, query } from "firebase/firestore";
+import { collection, getDocs, query, where } from "firebase/firestore";
 
 // Traer usuarios
 export async function getUsers(setState) {
-  let array = [];
   const q = query(collection(db, "users"));
   const users = await getDocs(q);
-
-  users.forEach((snap) => {
-    array.push({ ...snap.data(), id: snap.id });
-  });
+  const array = users.docs.map((snap) => ({ ...snap.data(), id: snap.id }));
   setState(() => array);
 }
 
 //Traer sábanas y toallas
 export async function getLinens(setState) {
-  let array = [];
-  const q = query(collection(db, "linen"));
+  const q = query(
+    collection(db, "linen"),
+    where("status", "not-in", ["Lavadero - en proceso", "Lavadero - listo"])
+  );
   const linens = await getDocs(q);
-
-  linens.forEach((snap) => {
-    array.push({ ...snap.data(), id: snap.id });
-  });
+  const array = linens.docs.map((snap) => ({ ...snap.data(), id: snap.id }));
   setState(() => array);
 }
